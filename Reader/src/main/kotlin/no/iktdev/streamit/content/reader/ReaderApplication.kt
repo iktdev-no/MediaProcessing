3f53510fd98075b2ca243facb895188eb5aa9598
Brage <bskjon@outlook.com>
2023-07-17 01:12:10 +0200

    Update

--- Reader/src/main/kotlin/no/iktdev/streamit/content/reader/ReaderApplication.kt
+++ Reader/src/main/kotlin/no/iktdev/streamit/content/reader/ReaderApplication.kt
@@ -2,15 +2,20 @@ package no.iktdev.streamit.content.reader
 
 import no.iktdev.streamit.content.reader.analyzer.PreferenceReader
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.context.ApplicationContext
 
 @SpringBootApplication
 class ReaderApplication
 
 val preference = PreferenceReader().getPreference()
+private var context: ApplicationContext? = null
+fun getContext(): ApplicationContext? {
+    return context
+}
+fun main(args: Array<String>) {
 
-fun main(array: Array<String>) {
-
-
+    context = runApplication<ReaderApplication>(*args)
 
 }
 
