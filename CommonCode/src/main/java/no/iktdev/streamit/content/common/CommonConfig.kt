d5d4542919c122e9d07fa5a7f4b9bc74b5b48779
Brage <bskjon@outlook.com>
2023-07-17 01:58:55 +0200

    Updated watchable folder

--- CommonCode/src/main/java/no/iktdev/streamit/content/common/CommonConfig.kt
+++ CommonCode/src/main/java/no/iktdev/streamit/content/common/CommonConfig.kt
@@ -4,6 +4,6 @@ import java.io.File
 
 object CommonConfig {
     var kafkaTopic: String = System.getenv("KAFKA_TOPIC") ?: "contentEvents"
-    var incomingContent: File? = if (!System.getenv("DIRECTORY_CONTENT_INCOMING").isNullOrEmpty()) File(System.getenv("DIRECTORY_CONTENT_INCOMING")) else null
+    var incomingContent: File? = if (!System.getenv("DIRECTORY_CONTENT_INCOMING").isNullOrBlank()) File(System.getenv("DIRECTORY_CONTENT_INCOMING")) else File("/src/input")
 
 }
