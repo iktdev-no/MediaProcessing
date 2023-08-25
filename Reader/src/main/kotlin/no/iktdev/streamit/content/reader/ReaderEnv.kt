d8f517f08b8ba153b9d0127c38f61dddea23335a
Brage <bskjon@outlook.com>
2023-08-25 02:32:01 +0200

    Nullable

--- Reader/src/main/kotlin/no/iktdev/streamit/content/reader/ReaderEnv.kt
+++ Reader/src/main/kotlin/no/iktdev/streamit/content/reader/ReaderEnv.kt
@@ -4,7 +4,7 @@ import java.io.File
 
 class ReaderEnv {
     companion object {
-        val metadataTimeOut: Long = System.getenv("TIMEOUT_READER_WAIT_FOR_METADATA").toLongOrNull() ?: 300000
+        val metadataTimeOut: Long = System.getenv("TIMEOUT_READER_WAIT_FOR_METADATA")?.toLongOrNull() ?: 300000
         val ffprobe: String = System.getenv("SUPPORTING_EXECUTABLE_FFPROBE") ?: "ffprobe"
         val encodePreference: File = File("/data/config/preference.json")
     }
