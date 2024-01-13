f6f5ce468768bbb2dd19f04867c78c6932cf32b5
Brage <brage.skjonborg@hotmail.no>
2024-01-13 13:48:06 +0100

    Updated

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -35,7 +35,6 @@ dependencies {
     implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
     implementation("org.jetbrains.exposed:exposed-java-time:$exposedVersion")
     implementation ("mysql:mysql-connector-java:8.0.29")
-    implementation(project(mapOf("path" to ":shared:kafka")))
 
 
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
