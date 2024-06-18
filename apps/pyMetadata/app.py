4d4e80a2b955c395f29f7cfacb8acafbbe0f6b13
bskjon <bskjon@outlook.com>
2024-06-18 19:23:57 +0200

    Changed path

--- apps/pyMetadata/app.py
+++ apps/pyMetadata/app.py
@@ -9,17 +9,17 @@ import json
 import time
 from fuzzywuzzy import fuzz
 
-from .algo.AdvancedMatcher import AdvancedMatcher
-from .algo.SimpleMatcher import SimpleMatcher
-from .algo.PrefixMatcher import PrefixMatcher
+from algo.AdvancedMatcher import AdvancedMatcher
+from algo.SimpleMatcher import SimpleMatcher
+from algo.PrefixMatcher import PrefixMatcher
 from clazz.shared import ConsumerRecord, MediaEvent, decode_key, decode_value, suppress_ignore, consume_on_key
 from clazz.KafkaMessageSchema import KafkaMessage, MessageDataWrapper
 from clazz.Metadata import Metadata
 from kafka import KafkaConsumer, KafkaProducer
 
-from .sources.anii import Anii
-from .sources.imdb import Imdb
-from .sources.mal import Mal
+from sources.anii import Anii
+from sources.imdb import Imdb
+from sources.mal import Mal
 
 # Konfigurer Kafka-forbindelsen
 bootstrap_servers = os.environ.get("KAFKA_BOOTSTRAP_SERVER") or "127.0.0.1:9092"
