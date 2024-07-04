31a47405dfdceab939d2b120c4fd3e1f23b4754b
bskjon <bskjon@outlook.com>
2024-07-04 16:52:25 +0200

    Naming

--- shared/common/src/main/kotlin/no/iktdev/mediaprocessing/shared/common/parsing/NameHelper.kt
+++ shared/common/src/main/kotlin/no/iktdev/mediaprocessing/shared/common/parsing/NameHelper.kt
@@ -7,6 +7,7 @@ object NameHelper {
     fun normalize(text: String): String {
         val normalized = Normalizer.normalize(text, Normalizer.Form.NFC)
         val result = normalized.replace("\\p{M}".toRegex(), "")
-        return StringUtils.stripAccents(result)
+        val cleaned = "[^A-Za-z0-9 -]".toRegex().replace(result, "")
+        return StringUtils.stripAccents(cleaned)
     }
 }
