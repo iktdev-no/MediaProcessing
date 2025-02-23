db7a82b3b7399f6168d2d16c1ef72015bdeb682c
bskjon <bskjon@outlook.com>
2025-02-23 03:29:48 +0100

    Attempting to fix black magic 2.5

--- apps/ui/src/main/kotlin/no/iktdev/mediaprocessing/ui/UIEnv.kt
+++ apps/ui/src/main/kotlin/no/iktdev/mediaprocessing/ui/UIEnv.kt
@@ -3,6 +3,6 @@ package no.iktdev.mediaprocessing.ui
 import java.io.File
 
 object UIEnv {
-    val socketEncoder: String = if (System.getenv("EncoderWs").isNullOrBlank()) System.getenv("EncoderWs") else "ws://encoder:8080"
+    val socketEncoder: String = System.getenv("EncoderWs")?.takeIf { it.isNotBlank() } ?: "ws://encoder:8080"
     val coordinatorUrl: String = System.getenv("Coordinator")?.takeIf { it.isNotBlank() } ?: "http://coordinator"
 }
