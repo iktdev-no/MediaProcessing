cec4e2aca21eaa71f88cb9a9f316fc997f989a16
bskjon <bskjon@outlook.com>
2025-02-23 18:53:11 +0100

    Now using mode

--- apps/coordinator/src/main/kotlin/no/iktdev/mediaprocessing/coordinator/controller/RequestEventController.kt
+++ apps/coordinator/src/main/kotlin/no/iktdev/mediaprocessing/coordinator/controller/RequestEventController.kt
@@ -21,57 +21,51 @@ class RequestEventController(@Autowired var coordinator: Coordinator) {
 
     @PostMapping("/convert")
     @ResponseStatus(HttpStatus.OK)
-    fun requestConvert(@RequestBody payload: String): ResponseEntity<String> {
-        var convert: EventRequest? = null
+    fun requestConvert(@RequestBody payload: EventRequest): ResponseEntity<String> {
         var referenceId: String?
         try {
-            convert = Gson().fromJson(payload, EventRequest::class.java)
-            val file = File(convert.file)
+            val file = File(payload.file)
             if (!file.exists()) {
-                return ResponseEntity.status(HttpStatus.NO_CONTENT).body(convert.file)
+                return ResponseEntity.status(HttpStatus.NO_CONTENT).body(payload.file)
             }
-            referenceId = coordinator.startProcess(file, ProcessType.FLOW, listOf(OperationEvents.CONVERT)).toString()
+            referenceId = coordinator.startProcess(file, payload.mode, listOf(OperationEvents.CONVERT)).toString()
 
         } catch (e: Exception) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Gson().toJson(convert))
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Gson().toJson(payload))
         }
         return ResponseEntity.ok(referenceId)
     }
 
     @PostMapping("/extract")
     @ResponseStatus(HttpStatus.OK)
-    fun requestExtract(@RequestBody payload: String): ResponseEntity<String> {
-        var request: EventRequest? = null
+    fun requestExtract(@RequestBody payload: EventRequest): ResponseEntity<String> {
         var referenceId: String?
         try {
-            request = Gson().fromJson(payload, EventRequest::class.java)
-            val file = File(request.file)
+            val file = File(payload.file)
             if (!file.exists()) {
-                return ResponseEntity.status(HttpStatus.NO_CONTENT).body(payload)
+                return ResponseEntity.status(HttpStatus.NO_CONTENT).body(Gson().toJson(payload))
             }
-            referenceId = coordinator.startProcess(file, ProcessType.MANUAL, listOf(OperationEvents.EXTRACT)).toString()
+            referenceId = coordinator.startProcess(file, payload.mode, listOf(OperationEvents.EXTRACT)).toString()
 
         } catch (e: Exception) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(payload)
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Gson().toJson(payload))
         }
         return ResponseEntity.ok(referenceId)
     }
 
     @PostMapping("/all")
     @ResponseStatus(HttpStatus.OK)
-    fun requestAll(@RequestBody payload: String): ResponseEntity<String> {
-        var request: EventRequest? = null
+    fun requestAll(@RequestBody payload: EventRequest): ResponseEntity<String> {
         var referenceId: String?
         try {
-            request = Gson().fromJson(payload, EventRequest::class.java)
-            val file = File(request.file)
+            val file = File(payload.file)
             if (!file.exists()) {
-                return ResponseEntity.status(HttpStatus.NO_CONTENT).body(payload)
+                return ResponseEntity.status(HttpStatus.NO_CONTENT).body(payload.file)
             }
-            referenceId = coordinator.startProcess(file, type = ProcessType.MANUAL).toString()
+            referenceId = coordinator.startProcess(file, type = payload.mode).toString()
 
         } catch (e: Exception) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(payload)
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Gson().toJson(payload))
         }
         return ResponseEntity.ok(referenceId)
     }
