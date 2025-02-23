53dea1c480864f2fca79d3cfb25a3d0c6daa6321
bskjon <bskjon@outlook.com>
2025-02-23 22:40:01 +0100

    Increased length

--- shared/common/src/main/kotlin/no/iktdev/mediaprocessing/shared/common/database/tables/tasks.kt
+++ shared/common/src/main/kotlin/no/iktdev/mediaprocessing/shared/common/database/tables/tasks.kt
@@ -8,7 +8,7 @@ import java.time.LocalDateTime
 
 object tasks: IntIdTable() {
     val referenceId: Column<String> = varchar("referenceId", 50)
-    val inputFile: Column<String?> = varchar("inputFile", 250).nullable()
+    val inputFile: Column<String?> = varchar("inputFile", 350).nullable()
     val status: Column<String?> = varchar("status", 10).nullable()
     val claimed: Column<Boolean> = bool("claimed").default(false)
     val claimedBy: Column<String?> = varchar("claimedBy", 100).nullable()
