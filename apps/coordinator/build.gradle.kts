931e60c301d10cb2570c671424a01cb404bfd3ab
Brage Skjønborg <bskjon@outlook.com>
2025-10-19 03:32:42 +0200

    Updated streamit db version

--- apps/coordinator/build.gradle.kts
+++ apps/coordinator/build.gradle.kts
@@ -38,7 +38,7 @@ dependencies {
     implementation("org.json:json:20210307")
 
     implementation("no.iktdev:exfl:0.0.16-SNAPSHOT")
-    implementation("no.iktdev.streamit.library:streamit-library-db:1.0-rc1")
+    implementation("no.iktdev.streamit.library:streamit-library-db:1.0-rc4")
 
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
