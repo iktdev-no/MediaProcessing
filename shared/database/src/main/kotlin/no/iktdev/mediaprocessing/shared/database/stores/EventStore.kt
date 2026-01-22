5dfa73af0816ec7ce36d0b0c24057454e801a129
Brage Skjønborg <bskjon@outlook.com>
2026-01-22 03:03:27 +0100

    Fixes + version

--- shared/database/src/main/kotlin/no/iktdev/mediaprocessing/shared/database/stores/EventStore.kt
+++ shared/database/src/main/kotlin/no/iktdev/mediaprocessing/shared/database/stores/EventStore.kt
@@ -15,7 +15,7 @@ object EventStore: EventStore {
     override fun getPersistedEventsAfter(timestamp: LocalDateTime): List<PersistedEvent> {
         val result = withTransaction {
             EventsTable.selectAll()
-                .where { EventsTable.persistedAt greater timestamp }
+                .where { EventsTable.persistedAt greaterEq timestamp }
                 .map {
                     PersistedEvent(
                         id = it[EventsTable.id].value.toLong(),
