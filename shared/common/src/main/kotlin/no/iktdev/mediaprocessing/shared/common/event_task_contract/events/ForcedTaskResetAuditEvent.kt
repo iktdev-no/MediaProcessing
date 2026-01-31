a49661736b9ad36eb82f153a3ef6c62d5c741c23
Brage Skjønborg <bskjon@outlook.com>
2026-02-01 00:56:52 +0100

    Signal event

--- shared/common/src/main/kotlin/no/iktdev/mediaprocessing/shared/common/event_task_contract/events/ForcedTaskResetAuditEvent.kt
+++ shared/common/src/main/kotlin/no/iktdev/mediaprocessing/shared/common/event_task_contract/events/ForcedTaskResetAuditEvent.kt
@@ -1,8 +1,8 @@
 package no.iktdev.mediaprocessing.shared.common.event_task_contract.events
 
-import no.iktdev.eventi.models.Event
+import no.iktdev.eventi.models.SignalEvent
 import java.util.*
 
 data class ForcedTaskResetAuditEvent(
     val taskId: UUID,
-): Event()
+): SignalEvent()
