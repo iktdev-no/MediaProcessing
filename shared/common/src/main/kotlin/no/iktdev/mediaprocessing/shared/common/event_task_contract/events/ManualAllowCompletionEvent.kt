ec20ff0b029be0473add1df68360099225899bbf
Brage Skjønborg <bskjon@outlook.com>
2026-01-31 21:44:37 +0100

    Corrected inheritance

--- shared/common/src/main/kotlin/no/iktdev/mediaprocessing/shared/common/event_task_contract/events/ManualAllowCompletionEvent.kt
+++ shared/common/src/main/kotlin/no/iktdev/mediaprocessing/shared/common/event_task_contract/events/ManualAllowCompletionEvent.kt
@@ -1,6 +1,6 @@
 package no.iktdev.mediaprocessing.shared.common.event_task_contract.events
 
-import no.iktdev.eventi.models.Event
+import no.iktdev.eventi.models.SignalEvent
 
-class ManualAllowCompletionEvent: Event() {
+class ManualAllowCompletionEvent: SignalEvent() {
 }
