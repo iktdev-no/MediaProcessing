d0bbe74931893233938381b82fd27a1ee51b6ecc
Brage Skjønborg <bskjon@outlook.com>
2026-04-01 00:56:54 +0200

    Using correct path

--- apps/coordinator/src/main/kotlin/no/iktdev/mediaprocessing/coordinator/controller/c2c/CpuLimitController.kt
+++ apps/coordinator/src/main/kotlin/no/iktdev/mediaprocessing/coordinator/controller/c2c/CpuLimitController.kt
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Mono
 
 @RestController
-@RequestMapping("/processer/cpu-limit")
+@RequestMapping("/system/cpu-limit")
 class CpuLimitController(
     private val processerClient: ProcesserClient
 ) {
