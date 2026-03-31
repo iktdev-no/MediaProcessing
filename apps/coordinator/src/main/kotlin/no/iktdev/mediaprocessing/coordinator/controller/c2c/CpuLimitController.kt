b78db2c4d6e3880c8180684525aee9da84889906
Brage Skjønborg <bskjon@outlook.com>
2026-04-01 01:41:55 +0200

    Wrong annotation...

--- apps/coordinator/src/main/kotlin/no/iktdev/mediaprocessing/coordinator/controller/c2c/CpuLimitController.kt
+++ apps/coordinator/src/main/kotlin/no/iktdev/mediaprocessing/coordinator/controller/c2c/CpuLimitController.kt
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Mono
@@ -23,7 +24,7 @@ class CpuLimitController(
     }
 
     @PostMapping()
-    fun setCpuLimit(@PathVariable limit: CPULimit): ResponseEntity<String> {
+    fun setCpuLimit(@RequestBody limit: CPULimit): ResponseEntity<String> {
         processerClient.setCpuLimit(limit)
         return ResponseEntity.ok("CPU limit updated to ${limit.limit}%")
     }
