438b4e8166860e75d38e8f331f65f8b449cd903c
Brage Skjønborg <brage@skjonborg.no>
2026-08-20 14:58:38 +0200

    Fixed3

--- apps/coordinator/src/main/kotlin/no/iktdev/mediaprocessing/coordinator/listeners/events/DetermineCollectionTaskCreateListener.kt
+++ apps/coordinator/src/main/kotlin/no/iktdev/mediaprocessing/coordinator/listeners/events/DetermineCollectionTaskCreateListener.kt
@@ -7,11 +7,15 @@ import no.iktdev.mediaprocessing.shared.common.event_task_contract.events.Comple
 import no.iktdev.mediaprocessing.shared.common.event_task_contract.events.DetermineCollectionTaskCreatedEvent
 import no.iktdev.mediaprocessing.shared.common.event_task_contract.events.MediaParsedInfoEvent
 import no.iktdev.mediaprocessing.shared.common.event_task_contract.events.MetadataSearchResultEvent
+import no.iktdev.mediaprocessing.shared.common.event_task_contract.events.OperationType
+import no.iktdev.mediaprocessing.shared.common.event_task_contract.events.StartProcessingEvent
 import no.iktdev.mediaprocessing.shared.common.event_task_contract.tasks.DetermineCollectionTask
 import no.iktdev.mediaprocessing.shared.common.getInstanceOf
 import no.iktdev.mediaprocessing.shared.common.ofTypes
+import no.iktdev.mediaprocessing.shared.common.requireEventValue
 import no.iktdev.mediaprocessing.shared.database.stores.TaskStore
 import org.springframework.stereotype.Component
+import kotlin.reflect.KClass
 
 @ListenerOrder(6)
 @Component
@@ -19,20 +23,26 @@ class DetermineCollectionTaskCreateListener : EventListener() {
 
     override fun allowDerivativeOnHistoricalEvent() = true
 
-    private val requiredEvents = listOf(
-        MediaParsedInfoEvent::class,
-        MetadataSearchResultEvent::class,
-    )
+
+    private fun getRequiredEvents(operations: Set<OperationType>): List<KClass<out Event>> {
+        return if (operations.contains(OperationType.MetadataSearch)) {
+            listOf(
+                MediaParsedInfoEvent::class,
+                MetadataSearchResultEvent::class,
+            )
+        } else listOf(MediaParsedInfoEvent::class)
+    }
 
     override fun onEvent(event: Event, history: List<Event>): Event? {
         val useEvents = history + event
-
+        val startOperations = useEvents.requireEventValue<StartProcessingEvent, Set<OperationType>> {it.data.operation}
         // Stop if completed
         if (useEvents.any { it is CompletedEvent }) return null
 
         // Avoid duplicates
         if (useEvents.any { it is DetermineCollectionTaskCreatedEvent }) return null
 
+        val requiredEvents = getRequiredEvents(startOperations);
         // Check if all required events exist
         val hasAllRequired = requiredEvents.all { type ->
             useEvents.any { type.isInstance(it) }
